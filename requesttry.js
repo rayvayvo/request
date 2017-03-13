be3148526081449c90b33f98acc9c596630054b2
Raven Ackerman <rayvayvo@gmail.com>
2017-03-13 21:12:31 +0000

    put files in project

---
+++ requesttry.js
@@ -0,0 +1,21 @@
+// request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {
+       console.log('NEWP! ERROR');                                // Note 2
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log(response.statusMessage);                     // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         // console.log(response.headers[''];
+       })
+       .pipe(fs.createWriteStream('./downloaded.jpg'));               // Note 4
+
+// Notes:
+// 1. `request.get` is equivalent to `request()`
+// 2. `request.on('error', callback)` handles any error
+// 3. `request.on('response, callback)` handles the response
+// 4. What is happening here?
